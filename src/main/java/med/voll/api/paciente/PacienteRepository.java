98e53b6a11e7c4f6c1c87556a1df556c428ec528
Rafael  Guimarães Esteque <rafael.esteque@dbccompany.com.br>
2024-01-29 18:48:12 -0300

    teste apache poi funcional

--- src/main/java/med/voll/api/paciente/PacienteRepository.java
+++ src/main/java/med/voll/api/paciente/PacienteRepository.java
@@ -4,8 +4,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.io.Serializable;
 import java.util.Optional;
 
-public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+public interface PacienteRepository extends JpaRepository<Paciente, Serializable> {
     Page<Paciente> findAllByAtivoTrue(Pageable paginacao);
 }
